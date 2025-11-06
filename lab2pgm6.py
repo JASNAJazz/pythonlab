424e5d43cd657376b8ca00975d2699769aab0c8f
JASNA <134054342+JASNAJazz@users.noreply.github.com>
2025-11-06 09:58:12 +0530

    Update lab2pgm6.py

--- lab2pgm6.py
+++ lab2pgm6.py
@@ -1,7 +1,22 @@
-list1= list(map(int,input("enter first list:").split()))
-list2= list(map(int,input("enter second list:").split()))
-print("same length?",len(list1)==len(list2))
-print("same sum?",sum(list1)==sum(list2))
-common=set(list1)& set(list2)
-print ("common value :", common if common else "none")
+list1 = list(map(int, input("Enter the first list: ").split()))
+list2 = list(map(int, input("Enter the second list: ").split()))
+
+if len(list1) == len(list2):
+    print("Both lists are of the same length")
+else:
+    print("Lists are not of the same length")
+
+if sum(list1) == sum(list2):
+    print("Both lists have the same sum")
+else:
+    print("Lists do not have the same sum")
+
+common = set(list1) & set(list2)
+
+if common:
+    print("Common values are:", common)
+else:
+    print("No common values")
+
+
 
